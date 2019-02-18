8dcbd76df458e95012717a8f760b6ca67c55fe29
Jack Lin <Jack@MacBook-Pro.local>
2019-02-18 16:38:17 +0800

    finished grep-infos.py

--- grep-infos.py
+++ grep-infos.py
@@ -1,3 +1,14 @@
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+import re
 for homo in SeqIO.parse("test.fastq", "fasta"):
-    print(homo.id + "\n")
+    pattern = re.compile(r"\|.+\|")
+    idv = pattern.findall(homo.id)
+    my_seq = Seq(str(homo.seq), IUPAC.protein)
+    ids = re.sub(r"\[\'\||\|\'\]","",str(idv))
+    for index, letter in enumerate(my_seq):
+        AA = ['F','L','S','Y','C','W','P','H','Q','I','M','T','N','K','R','V','A','D','E','G']
+        AA.remove(str(letter))
+        for Var in AA:
+            print ids, str(index+1), letter, Var
